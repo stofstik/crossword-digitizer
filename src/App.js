66a51368020d749b0d2a1f101d54a4a529d5b93a
stofstik <verstegen.daan@gmail.com>
2017-03-06 22:51:46 +0100

    clean up

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import _ from 'underscore';
 import React, { Component } from 'react';
 import './App.css';
 import { CharInput } from './components/CharInput';
-import ImageProcessor from './lib/imageProcessor.js';
 
 class App extends Component {
   constructor(props) {
@@ -33,7 +32,6 @@ class App extends Component {
       color[1] === 255 &&
       color[2] === 255 &&
       color[3] === 255
-    console.log(color, bool)
     return bool
   }
 
@@ -42,19 +40,75 @@ class App extends Component {
       color[1] === 0 &&
       color[2] === 0 &&
       color[3] === 255
-    console.log(color, bool)
     return bool
   }
 
-  findSquareSize(y) {
+  findCenter(x, y) {
+    console.log('findCenter');
+    let color = this.ctx.getImageData(x, y, 1, 1).data;
+    const startX = x;
+    const startY = y;
+    console.log(startX, startY, color);
+    let right, left, top, bottom;
+    // Find left border
+    for(let x = 0; x < 30; x++) {
+      const pos = startX - x;
+      color = this.ctx.getImageData(pos, startY, 1, 1).data;
+      if(!this.isWhite(color)) {
+        left = pos;
+        console.log('left border at', left);
+        break;
+      }
+    }
+    // Find right border
+    for(let x = 0; x < 30; x++) {
+      const pos = startX + x;
+      color = this.ctx.getImageData(pos, startY, 1, 1).data;
+      if(!this.isWhite(color)) {
+        right = pos;
+        console.log('right border at', right);
+        break;
+      }
+    }
+    // Find top border
+    for(let y = 0; y < 30; y++) {
+      const pos = startY - y;
+      color = this.ctx.getImageData(startX, pos, 1, 1).data;
+      if(!this.isWhite(color)) {
+        top = pos;
+        console.log('top border at', top);
+        break;
+      }
+    }
+    // Find bottom border
+    for(let y = 0; y < 30; y++) {
+      const pos = startY + y;
+      color = this.ctx.getImageData(startX, pos, 1, 1).data;
+      if(!this.isWhite(color)) {
+        bottom = pos;
+        console.log('bottom border at', bottom);
+        break;
+      }
+    }
+    const height = bottom - top;
+    const width  = right  - left;
+    console.log(height, width);
+    const centerX = left + width  / 2;
+    const centerY = top  + height / 2;
+    console.log(centerX, centerY);
+    this.setState( { size: width - 4 } )
+    if(width < 50 && height < 50) {
+      return { x: centerX, y: centerY };
+    }
+  }
+
+  estimateSizeByPixelRow(y) {
     console.log('finding square size');
     console.log(this.canvas.width)
     console.log(this.canvas.height)
     const w = this.canvas.width;
-    const h = this.canvas.height;
     let sizes     = []
     let wCounter  = 0
-    let bCounter  = 0
     let lastColor = 'white';
     // for(let y = 0; y < h; y++) {
     for(let x = 0; x < w; x++) {
